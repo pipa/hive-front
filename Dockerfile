e8cce793316d7ae2a5a56c8bdafcd57ca545c69f
Luis Matute <luis.matute@me.com>
2018-03-04 18:41:51 +0100

    - [add] Dockerfile for project

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:9.7.1
+
+# Create directory and change to it
+WORKDIR /webroot/default/
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+RUN npm install
---
+++ index.js
@@ -0,0 +1,17 @@
+// requiring the HTTP interfaces in node
+var http = require('http');
+
+// create an http server to handle requests and response
+http.createServer(function (req, res) {
+
+  // sending a response header of 200 OK
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // print out Hello World
+  res.end('Im the front!\n');
+
+// use port 9999
+}).listen(9999);
+
+
+console.log('Server running on port 9999.');
